896fb8bd49a8c08ea010aaee7ba100618a32f462
chandu00081 <chandu00081@gmail.com>
2023-02-08 19:44:52 +0530

    using single player

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sneakladder
+{
+    internal class Sneak
+    {
+        public void player()
+        {
+            int Num_Player = 1;
+            int Start_Position = 0;
+
+            Console.WriteLine($" Number Of Players is :{Num_Player}");
+            Console.WriteLine($" Starting Position is :{Start_Position}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sneak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake and Ladder Problem ");
+            Sneak Cobra = new Sneak();
+            Cobra.player();
+        }
+    }
+}
