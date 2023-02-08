44fc28d3baab68c5ee69ff5ad664f27b8a0ca2b1
chandu00081 <chandu00081@gmail.com>
2023-02-08 20:12:07 +0530

    Roll Die

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sneakladder
+{
+    internal class Rolldie
+    {
+        public void player()
+        {
+ 
+               Random random = new Random();
+               int Die = random.Next(1, 6);
+               Console.WriteLine($"Roll Die is : {Die}");
+            
+        }
+    }
+}
