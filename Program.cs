4a86025e3d875195e00017997176aeaa6ba937bf
chandu00081 <chandu00081@gmail.com>
2023-02-08 20:42:44 +0530

    winning position

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sneakladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake and Ladder Problem ");
+            Rolldie Cobra = new Rolldie();
+            Cobra.player();
+
+        }
+    }
+}
