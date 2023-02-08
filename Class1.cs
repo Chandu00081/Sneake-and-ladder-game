797bab5029cd6faca28fa9fecb0886852b35b22e
chandu00081 <chandu00081@gmail.com>
2023-02-08 20:26:18 +0530

    opearion check

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sneakladder
+{
+    internal class Rolldie
+    {
+        public const int Ahead_Move = 1;
+        public const int Behind_Move = 2;
+        public const int No_Move = 3;
+        public void player()
+        {
+            int position = 0;
+            Random random = new Random();
+            int MovingCheck = random.Next(1, 6);
+            int StepCheck = random.Next(1, 3);
+            Console.WriteLine($"Roll Die is : {MovingCheck}");
+            Console.WriteLine($"Moving Check is : {StepCheck}");
+
+            switch (StepCheck)
+            {
+                case Ahead_Move:
+
+                    position = position + MovingCheck;
+                    Console.WriteLine("Ahead Move");
+
+                    break;
+                case Behind_Move:
+
+                    position = position - MovingCheck;
+                    Console.WriteLine("Behind Move");
+
+                    break;
+                case No_Move:
+
+                    position = position + MovingCheck;
+                    Console.WriteLine("No Move");
+
+                    break;
+            }
+            Console.WriteLine("Step Count is :" + position);
+        }
+    }
+}
